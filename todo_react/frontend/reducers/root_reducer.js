0d6dbb7707fd4e3d9be0121cdc4443693fb72c64
miker704 <miker704@gmail.com>
2022-06-19 13:03:45 -0400

    prac update

--- todo_react/frontend/reducers/root_reducer.js
+++ todo_react/frontend/reducers/root_reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import todosReducer from "./todos_Reducer.js";
 import stepsReducer from "./steps_reducer.js";
+import errorsReducer from "./error_reducer.js";
 
 const rootReducer = combineReducers({
     todos: todosReducer,
