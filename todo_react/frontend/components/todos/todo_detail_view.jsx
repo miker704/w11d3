442cf9cb4f155a8a4f05826f855d3edfe99b2caa
miker704 <miker704@gmail.com>
2022-06-18 22:57:00 -0400

    prac update

---
+++ todo_react/frontend/components/todos/todo_detail_view.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+class TodoDetailView extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+
+export default TodoDetailView;
