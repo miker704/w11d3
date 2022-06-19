c53bc253a2eb2012e8b471fa506e99d0ae9951c1
miker704 <miker704@gmail.com>
2022-06-18 23:22:58 -0400

    prac update

---
+++ todo_react/frontend/components/steps/step_list_container.jsx
@@ -0,0 +1,16 @@
+import { connect } from "react-redux";
+import StepList from "./step_list.js";
+
+
+const mSTP = state => ({
+
+});
+
+const mDTP = dispatch => ({
+
+});
+
+
+
+const StepListContainer = connect(mSTP, mDTP)(StepList);
+export default StepListContainer;
