9bf8dd4715b3234ca6b21f54bec64468807c2da4
miker704 <miker704@gmail.com>
2022-06-19 13:05:17 -0400

    prac update

--- todo_react/frontend/reducers/error_reducer.js
+++ todo_react/frontend/reducers/error_reducer.js
@@ -1,8 +1,16 @@
 import { RECEIVE_ERRORS, REMOVE_ERRORS } from "../actions/error_actions";
 
 
- const errorsReducer = (state =[], action) => {
+const errorsReducer = (state = [], action) => {
+    Object.freeze(state);
+    switch (action.type) {
+        case RECEIVE_ERRORS:
+            return action.errors;
+        case REMOVE_ERRORS:
+            return [];
+        default:
+            return state;
+    }
+}
 
- }
-
- export default errorsReducer;
+export default errorsReducer;
