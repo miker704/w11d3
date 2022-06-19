17a178bd387f78358e7d0d25de0a10ecf0f1ebf4
miker704 <miker704@gmail.com>
2022-06-19 13:53:12 -0400

    prac update

--- todo_react/frontend/components/steps/step_list_item_container.jsx
+++ todo_react/frontend/components/steps/step_list_item_container.jsx
@@ -2,9 +2,7 @@ import { connect } from "react-redux";
 import StepListItem from "./step_list_item.jsx";
 import { removeStep, receiveStep } from "../../actions/step_actions.js";
 
-const mSTP = state => ({
-    
-});
+
 
 const mDTP = (dispatch,{step}) => ({
 removeStep : ()=> dispatch(removeStep(step)),
@@ -13,5 +11,5 @@ receiveStep: step => dispatch(receiveStep(step))
 
 
 
-const StepListItemContainer = connect(mSTP, mDTP)(StepListItem);
+const StepListItemContainer = connect(null, mDTP)(StepListItem);
 export default StepListItemContainer;
