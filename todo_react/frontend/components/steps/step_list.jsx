367c050e500ba9b93e00329699eaecd98ab5ba92
miker704 <miker704@gmail.com>
2022-06-19 14:04:13 -0400

    prac update

--- todo_react/frontend/components/steps/step_list.jsx
+++ todo_react/frontend/components/steps/step_list.jsx
@@ -2,19 +2,12 @@ import React from "react";
 import { receiveStep } from "../../actions/step_actions";
 import StepForm from "./step_form";
 import StepListItemContainer from "./step_list_item_container.jsx";
-class StepList extends React.Component{
-    constructor(props){
-        super(props);
-    }
-    render(){
 
-        const stepList = ({steps,todo_id, receiveStep}) =>{
+        const StepList = ({steps,todo_id, receiveStep}) =>{
             const stepItems = steps.map((step) => {
                 return <StepListItemContainer key={step.id} step={step}/>
             })
-        };
-
-
+        
 
         return (
             <div>
@@ -24,7 +17,6 @@ class StepList extends React.Component{
                  <StepForm todo_id={todo_id} receiveStep={receiveStep}/>
             </div>
         )
-    }
-}
+        };
 
 export default StepList;
