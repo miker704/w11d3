7ce9b79662d93e166ede30575c91aedb8ba79484
miker704 <miker704@gmail.com>
2022-06-19 13:40:23 -0400

    prac update

--- todo_react/frontend/components/steps/step_form.jsx
+++ todo_react/frontend/components/steps/step_form.jsx
@@ -2,37 +2,37 @@ import React from "react";
 
 
 
-class StepForm extends React.Component{
-    constructor(props){
+class StepForm extends React.Component {
+    constructor(props) {
         super(props);
-        this.state ={title:"",body:"",done:false,todo_id:this.props.todo_id}
+        this.state = { title: "", body: "", done: false, todo_id: this.props.todo_id }
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
 
     update(field) {
         return e => this.setState({ [field]: e.currentTarget.value });
-    
+
     }
     handleSubmit(e) {
         e.preventDefault();
-        const step = Object.assign({}, this.state,{ id: uniqueId() });
+        const step = Object.assign({}, this.state, { id: uniqueId() });
         this.props.receiveStep(step);
-        this.setState({title: "",body:""});
+        this.setState({ title: "", body: "" });
     }
-    render(){
-        return(
+    render() {
+        return (
             <form onSubmit={this.handleSubmit}>
                 <label>Create New Step</label> <br />
                 <label>Title: <input id="titleinput" type="text" onChange={this.update('title')} value={this.state.title} /> <br /></label>
-                
-                <label>Description: <textarea id="bodyinput" cols="20" rows="1" onChange={this.update('body') }value={this.state.body}></textarea> <br /></label>
-                
+
+                <label>Description: <textarea id="bodyinput" cols="20" rows="1" onChange={this.update('body')} value={this.state.body}></textarea> <br /></label>
+
                 <button className="create-button">Create Step</button>
 
             </form>
         )
-    } 
+    }
 }
 
 export default StepForm;
