85ea682875dfc0eca60ee80798ac4fc9d629c2f3
miker704 <miker704@gmail.com>
2022-06-18 23:25:00 -0400

    prac update

---
+++ todo_react/frontend/components/steps/step_list_item.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+class StepListItem extends React.Component{
+    constructor(props){
+        super(props);
+
+    }
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    } 
+}
+
+export default StepListItem;
