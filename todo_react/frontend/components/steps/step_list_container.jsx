7e923d7217e7648f5084d83a5a6470a8c1a0822b
miker704 <miker704@gmail.com>
2022-06-19 00:23:41 -0400

    prac update

--- todo_react/frontend/components/steps/step_list_container.jsx
+++ todo_react/frontend/components/steps/step_list_container.jsx
@@ -1,13 +1,15 @@
 import { connect } from "react-redux";
 import StepList from "./step_list.js";
 
-
-const mSTP = state => ({
-
+import { stepsByTodoId } from "../../reducers/selectors.js";
+import { receiveStep } from "../../actions/step_actions.js";
+const mSTP = (state,{todo_id}) => ({
+    steps: stepsByTodoId(state,todo_id),
+    todo_id
 });
 
 const mDTP = dispatch => ({
-
+    receiveStep: step => dispatch(receiveStep(step))
 });
 
 
