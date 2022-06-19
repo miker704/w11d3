e9feab995ecbe1d1ab50251b99755c97ada1271d
miker704 <miker704@gmail.com>
2022-06-19 12:00:36 -0400

    prac update

--- todo_react/frontend/components/steps/step_form.jsx
+++ todo_react/frontend/components/steps/step_form.jsx
@@ -5,7 +5,19 @@ import React from "react";
 class StepForm extends React.Component{
     constructor(props){
         super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
+
+    update(field) {
+        return e => this.setState({ [field]: e.currentTarget.value });
+    
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        const step = Object.assign({}, this.state,{ id: uniqueId() });
+        this.props.receiveStep(step);
+        this.setState({title: "",body:""});
     }
     render(){
         return(
