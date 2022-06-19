9d9d9d8391e03456af7058847fcb533ae253d334
miker704 <miker704@gmail.com>
2022-06-19 12:16:33 -0400

    prac update

--- todo_react/frontend/components/todos/todo_detail_view.jsx
+++ todo_react/frontend/components/todos/todo_detail_view.jsx
@@ -9,7 +9,7 @@ class TodoDetailView extends React.Component{
         return(
             <div>
                 <p className="todo-body">{todo.body}</p>
-                <StepListContainer todo_id={todo_id}/>
+                <StepListContainer todo_id={todo.id}/>
                 <button className="delete-button" onClick={removeTodo}>Delete Todo</button>
             </div>
         )
