5c3968436992e75e87454e913e4eb91ad4c683c7
miker704 <miker704@gmail.com>
2022-06-19 13:17:56 -0400

    prac update

--- todo_react/frontend/actions/step_actions.js
+++ todo_react/frontend/actions/step_actions.js
@@ -1,4 +1,4 @@
-
+import * as StepAPIUtil from '../util/step_api_util';
 
 export const RECEIVE_STEPS = "RECEIVE_STEPS";
 
@@ -33,3 +33,28 @@ export const removeStep = (step) => {
 
 
 };
+
+
+
+//async 
+export const requestSteps = todoId => dispatch => (
+	StepAPIUtil.fetchSteps(todoId)
+	.then(steps => dispatch(receiveSteps(steps)))
+  );
+  
+  export const createStep = (todoId, step) => dispatch => (
+	StepAPIUtil.createStep(todoId, step)
+	.then(step => dispatch(receiveStep(step)))
+  );
+  
+  
+  export const updateStep = step => dispatch => (
+	StepAPIUtil.updateStep(step)
+	.then(step => dispatch(receiveStep(step)))
+  );
+  
+  export const destroyStep = step => dispatch => (
+	StepAPIUtil.destroyStep(step)
+	.then(step => dispatch(removeStep(step)))
+  );
+  
