b2d8aae78dac8eaf2aafbde0f35831fa89da5de1
miker704 <miker704@gmail.com>
2022-06-19 12:09:06 -0400

    prac update

--- todo_react/frontend/components/steps/step_list_item.jsx
+++ todo_react/frontend/components/steps/step_list_item.jsx
@@ -8,6 +8,20 @@ class StepListItem extends React.Component{
         this.toggleDetail = this.toggleDetail.bind(this);
         this.toggleStep = this.toggleStep.bind(this);
     }
+
+    
+    toggleDetail(e) {
+        e.preventDefault();
+        this.setState({ detail: !this.state.detail });
+
+    }
+    toggleTodo(e) {
+        e.preventDefault();
+        const toggledTodo = Object.assign({}, this.props.todo, { done: !this.props.todo.done })
+        this.props.receiveTodo(toggledTodo);
+    }
+
+
     render(){
         return(
             <div>
